395e77f02082bb04148908308d64422a38c83f2a
Sergey Berezin <sergey@sergeyberezin.com>
2022-03-23 15:01:25 -0700

    DB: implement writing & reading tickers and daily data (#15)

--- db/db.go
+++ db/db.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"encoding/gob"
 	"os"
+	"path/filepath"
+	"sync"
 
 	"github.com/stockparfait/errors"
 )
@@ -68,30 +70,175 @@ func readGob(fileName string, v interface{}) error {
 	return nil
 }
 
-// ResampledFrequency is enum for the available resample rates.
-type ResampledFrequency int
-
-// ResampledFrequency values.
-const (
-	Weekly ResampledFrequency = iota
-	Monthly
-	Quarterly
-)
-
 type Database struct {
-	cachePath string
-	tickers   map[string]TickerRow
-	weekly    map[string][]ResampledRow
-	monthly   map[string][]ResampledRow
-	quarterly map[string][]ResampledRow
+	cachePath    string
+	tickers      map[string]TickerRow
+	actions      map[string][]ActionRow
+	weekly       map[string][]ResampledRow
+	monthly      map[string][]ResampledRow
+	quarterly    map[string][]ResampledRow
+	tickersOnce  sync.Once
+	tickersError error
+	actionsOnce  sync.Once
+	actionsError error
+	mkdirOnce    sync.Once
+	mkdirError   error
 }
 
 func NewDatabase(cachePath string) *Database {
 	return &Database{
 		cachePath: cachePath,
 		tickers:   make(map[string]TickerRow),
+		actions:   make(map[string][]ActionRow),
 		weekly:    make(map[string][]ResampledRow),
 		monthly:   make(map[string][]ResampledRow),
 		quarterly: make(map[string][]ResampledRow),
 	}
+}
+
+func (db *Database) tickersFile() string {
+	return filepath.Join(db.cachePath, "tickers.gob")
+}
+
+func (db *Database) actionsFile() string {
+	return filepath.Join(db.cachePath, "actions.gob")
+}
+
+func (db *Database) pricesDir() string {
+	return filepath.Join(db.cachePath, "prices")
+}
+
+func (db *Database) pricesFile(ticker string) string {
+	return filepath.Join(db.pricesDir(), ticker+".gob")
+}
+
+func (db *Database) cacheTickers() error {
+	db.tickersOnce.Do(func() {
+		if err := readGob(db.tickersFile(), &db.tickers); err != nil {
+			db.tickersError = errors.Annotate(
+				err, "failed to load %s", db.tickersFile())
+		}
+	})
+	return db.tickersError
+}
+
+func (db *Database) cacheActions() error {
+	db.actionsOnce.Do(func() {
+		if err := readGob(db.actionsFile(), &db.actions); err != nil {
+			db.actionsError = errors.Annotate(
+				err, "failed to load %s", db.actionsFile())
+		}
+	})
+	return db.actionsError
+}
+
+func (db *Database) createDirs() error {
+	db.mkdirOnce.Do(func() {
+		if err := os.MkdirAll(db.pricesDir(), os.ModeDir|0755); err != nil {
+			db.mkdirError = errors.Annotate(
+				err, "failed to create %s", db.pricesDir())
+		}
+	})
+	return db.mkdirError
+}
+
+// WriteTickers saves the tickers table to the DB file.
+func (db *Database) WriteTickers(tickers map[string]TickerRow) error {
+	if err := db.createDirs(); err != nil {
+		return errors.Annotate(err, "failed to create DB directories")
+	}
+	if err := writeGob(db.tickersFile(), tickers); err != nil {
+		return errors.Annotate(err, "failed to write '%s'", db.tickersFile())
+	}
+	return nil
+}
+
+// WriteActions saves the actions table to the DB file. Actions are indexed by
+// ticker, and for each ticker actions are assumed to be sorted by date.
+func (db *Database) WriteActions(actions map[string][]ActionRow) error {
+	if err := db.createDirs(); err != nil {
+		return errors.Annotate(err, "failed to create DB directories")
+	}
+	if err := writeGob(db.actionsFile(), actions); err != nil {
+		return errors.Annotate(err, "failed to write '%s'", db.actionsFile())
+	}
+	return nil
+}
+
+// WritePrices saves the ticker prices to the DB file.  Prices are assumed to be
+// sorted by date.
+func (db *Database) WritePrices(ticker string, prices []PriceRow) error {
+	if err := db.createDirs(); err != nil {
+		return errors.Annotate(err, "failed to create DB directories")
+	}
+	if err := writeGob(db.pricesFile(ticker), prices); err != nil {
+		return errors.Annotate(err, "failed to write '%s'", db.pricesFile(ticker))
+	}
+	return nil
+}
+
+// TickerRow for the given ticker. It's an error if a ticker is not in DB.
+// Tickers are cached in memory upon the first call. Go routine safe.
+func (db *Database) TickerRow(ticker string) (TickerRow, error) {
+	if err := db.cacheTickers(); err != nil {
+		return TickerRow{}, errors.Annotate(err, "failed to load tickers")
+	}
+	r, ok := db.tickers[ticker]
+	if !ok {
+		return TickerRow{}, errors.Reason("no such ticker: %s", ticker)
+	}
+	return r, nil
+}
+
+// Tickers returns the list of tickers satisfying TickerRow data constraints.
+// Tickers are cached in memory upon the first call. Go routine safe.
+func (db *Database) Tickers(c *Constraints) ([]string, error) {
+	if err := db.cacheTickers(); err != nil {
+		return nil, errors.Annotate(err, "failed to load tickers")
+	}
+	tickers := []string{}
+	for t, r := range db.tickers {
+		if c.CheckTicker(t) && c.CheckTickerRow(r) {
+			tickers = append(tickers, t)
+		}
+	}
+	return tickers, nil
+}
+
+// Actions for ticker satisfying the constraints, sorted by date. Actions for
+// all tickers are cached in memory upon the first call. Go routine safe.
+func (db *Database) Actions(ticker string, c *Constraints) ([]ActionRow, error) {
+	if err := db.cacheActions(); err != nil {
+		return nil, errors.Annotate(err, "failed to load actions")
+	}
+	actions, ok := db.actions[ticker]
+	if !ok {
+		return nil, errors.Reason("no actions found for ticker %s", ticker)
+	}
+	res := []ActionRow{}
+	for _, a := range actions {
+		if c.CheckAction(a) {
+			res = append(res, a)
+		}
+	}
+	return res, nil
+}
+
+// Prices for ticker satilfying constraints, sorted by date. Prices are NOT
+// cached in memory, and are read from disk every time. It is probably go
+// routine safe, if the underlying OS allows to open and read the same file
+// multiple times from the same process. Reading different tickers is definitely
+// safe in parallel.
+func (db *Database) Prices(ticker string, c *Constraints) ([]PriceRow, error) {
+	prices := []PriceRow{}
+	if err := readGob(db.pricesFile(ticker), &prices); err != nil {
+		return nil, errors.Annotate(err, "failed to read prices for %s", ticker)
+	}
+	res := []PriceRow{}
+	for _, p := range prices {
+		if c.CheckPrice(p) {
+			res = append(res, p)
+		}
+	}
+	return res, nil
 }
--- db/db_test.go
+++ db/db_test.go
@@ -32,35 +32,78 @@ func TestDB(t *testing.T) {
 		So(tmpdirErr, ShouldBeNil)
 	})
 
-	Convey("writeGob / readGob work", t, func() {
-		Convey("for a slice of struct", func() {
-			f := filepath.Join(tmpdir, "slice.gob")
-			orig := []PriceRow{
-				TestPrice(NewDate(2019, 1, 1), 10.0, 1000.0),
-				TestPrice(NewDate(2019, 1, 2), 11.0, 2000.0),
-			}
-			So(writeGob(f, orig), ShouldBeNil)
-			var res []PriceRow
-			So(readGob(f, &res), ShouldBeNil)
-			So(res, ShouldResemble, orig)
+	Convey("Data access methods", t, func() {
+		dbPath := filepath.Join(tmpdir, "db")
+		tickers := map[string]TickerRow{
+			"A": {},
+			"B": {},
+		}
+		actions := map[string][]ActionRow{
+			"A": {
+				TestAction(NewDate(2019, 1, 1), 1.0, 1.0, true),
+			},
+			"B": {
+				TestAction(NewDate(2019, 1, 1), 1.0, 1.0, true),
+				TestAction(NewDate(2020, 1, 1), 1.0, 1.0, false),
+			},
+		}
+		pricesA := []PriceRow{
+			TestPrice(NewDate(2019, 1, 1), 10.0, 1000.0),
+			TestPrice(NewDate(2019, 1, 2), 11.0, 1100.0),
+			TestPrice(NewDate(2019, 1, 3), 12.0, 1200.0),
+		}
+		pricesB := []PriceRow{
+			TestPrice(NewDate(2019, 1, 1), 100.0, 100.0),
+			TestPrice(NewDate(2019, 1, 2), 110.0, 110.0),
+			TestPrice(NewDate(2019, 1, 3), 120.0, 120.0),
+		}
+
+		Convey("write methods work", func() {
+			db := NewDatabase(dbPath)
+			So(db.WriteTickers(tickers), ShouldBeNil)
+			So(db.WriteActions(actions), ShouldBeNil)
+			So(db.WritePrices("A", pricesA), ShouldBeNil)
+			So(db.WritePrices("B", pricesB), ShouldBeNil)
 		})
 
-		Convey("for a map of slices of struct", func() {
-			f := filepath.Join(tmpdir, "slice.gob")
-			orig := map[string][]ActionRow{
-				"A1": {
-					TestAction(NewDate(2019, 1, 1), 1.0, 1.0, true),
-					TestAction(NewDate(2020, 1, 1), 1.0, 1.0, false),
-				},
-				"A2": {
-					TestAction(NewDate(2018, 1, 1), 0.99, 1.0, true),
-					TestAction(NewDate(2018, 6, 1), 1.0, 0.5, true),
-				},
-			}
-			So(writeGob(f, orig), ShouldBeNil)
-			res := make(map[string][]ActionRow)
-			So(readGob(f, &res), ShouldBeNil)
-			So(res, ShouldResemble, orig)
+		Convey("ticker access methods work", func() {
+			db := NewDatabase(dbPath)
+			r, err := db.TickerRow("A")
+			So(err, ShouldBeNil)
+			So(r, ShouldResemble, tickers["A"])
+
+			r, err = db.TickerRow("B")
+			So(err, ShouldBeNil)
+			So(r, ShouldResemble, tickers["B"])
+
+			r, err = db.TickerRow("UNKNOWN")
+			So(err, ShouldNotBeNil)
+
+			ts, err := db.Tickers(NewConstraints().Ticker("A", "OTHER"))
+			So(err, ShouldBeNil)
+			So(ts, ShouldResemble, []string{"A"})
+		})
+
+		Convey("action access methods work", func() {
+			db := NewDatabase(dbPath)
+			a, err := db.Actions("A", NewConstraints())
+			So(err, ShouldBeNil)
+			So(a, ShouldResemble, actions["A"])
+
+			a, err = db.Actions("B", NewConstraints().EndAt(NewDate(2019, 6, 1)))
+			So(err, ShouldBeNil)
+			So(a, ShouldResemble, actions["B"][:1])
+		})
+
+		Convey("price access methods work", func() {
+			db := NewDatabase(dbPath)
+			p, err := db.Prices("A", NewConstraints())
+			So(err, ShouldBeNil)
+			So(p, ShouldResemble, pricesA)
+
+			p, err = db.Prices("B", NewConstraints().StartAt(NewDate(2019, 1, 2)))
+			So(err, ShouldBeNil)
+			So(p, ShouldResemble, pricesB[1:])
 		})
 	})
 }
