b85d1f288ccf4f64a5c586ed906f4df9ff222632
Sergey Berezin <sergey@sergeyberezin.com>
2022-06-11 15:50:10 -0700

    Implement Plot API (#54)

---
+++ plot/plot.go
@@ -0,0 +1,360 @@
+// Copyright 2022 Stock Parfait
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plot
+
+import (
+	"fmt"
+
+	"github.com/stockparfait/errors"
+
+	"github.com/stockparfait/stockparfait/db"
+	"github.com/stockparfait/stockparfait/stats"
+)
+
+// Kind is an enum for different kinds of plots; currently time series and
+// arbitrary (x, y) plots, such as curves or scatter plots.
+type Kind int
+
+// Values of Kind.
+const (
+	SeriesKind Kind = iota
+	XYKind
+)
+
+func (k Kind) String() string {
+	switch k {
+	case SeriesKind:
+		return "SeriesKind"
+	case XYKind:
+		return "XYKind"
+	default:
+		return fmt.Sprintf("<Undefined Kind: %d>", k)
+	}
+}
+
+// ChartType is an enum of different ways to plot data: as a connected solid or
+// dashed line, or as individual dots for scatter plots.
+type ChartType int
+
+// Values of ChartType.
+const (
+	ChartLine ChartType = iota
+	ChartDashed
+	ChartScatter
+)
+
+// Plot holds data and configuration of a single plot.
+type Plot struct {
+	Kind      Kind
+	X         []float64 // when Kind = XYKind
+	Y         []float64
+	Dates     []db.Date // when Kind = SeriesKind
+	YLabel    string    // value label on the Y axis
+	Legend    string    // name in the legend
+	ChartType ChartType
+}
+
+// NewSeriesPlot creates an instance of a time series plot.
+func NewSeriesPlot(ts *stats.Timeseries) *Plot {
+	return &Plot{
+		Kind:   SeriesKind,
+		Y:      ts.Data(),
+		Dates:  ts.Dates(),
+		YLabel: "values",
+		Legend: "Unnamed",
+	}
+}
+
+// NewXYPlot creates an instance of an untimed plot. Panics if the slices x and
+// y don't have the same length.
+func NewXYPlot(x, y []float64) *Plot {
+	if len(x) != len(y) {
+		panic(errors.Reason("len(x)=%d != len(y)=%d", len(x), len(y)))
+	}
+	return &Plot{
+		Kind:   XYKind,
+		X:      x,
+		Y:      y,
+		YLabel: "values",
+		Legend: "Unnamed",
+	}
+}
+
+// SetYLabel of the plot - used as the value label on the Y axis.
+func (p *Plot) SetYLabel(label string) *Plot {
+	p.YLabel = label
+	return p
+}
+
+// SetLegend of the plot - used as the plot's name in the legend.
+func (p *Plot) SetLegend(legend string) *Plot {
+	p.Legend = legend
+	return p
+}
+
+// SetChartType - how to plot the data.
+func (p *Plot) SetChartType(t ChartType) *Plot {
+	p.ChartType = t
+	return p
+}
+
+// GetTimeseries from a series Plot. Panics if Kind != SeriesKind.
+func (p *Plot) GetTimeseries() *stats.Timeseries {
+	if p.Kind != SeriesKind {
+		panic(errors.Reason("Kind is not SeriesKind"))
+	}
+	return stats.NewTimeseries().Init(p.Dates, p.Y)
+}
+
+// GetXY extracts X and Y slices from an untimed Plot. Panics if Kind != XYKind.
+func (p *Plot) GetXY() ([]float64, []float64) {
+	if p.Kind != XYKind {
+		panic(errors.Reason("kind %s is not XYKind", p.Kind))
+	}
+	return p.X, p.Y
+}
+
+// Graph is a container for Plots, and visually it corresponds to a single (X,
+// Y) or (Date, Y) chart where these plots are displayed.
+type Graph struct {
+	Kind       Kind    // each graph can only display one kind of plots
+	Name       string  // unique internal name of the graph
+	Title      string  // user visible graph title; defaults to Name
+	XLabel     string  // label on the X axis
+	YLogScale  bool    // whether both Y axis are log-scale
+	PlotsRight []*Plot // plots using the right Y axis
+	PlotsLeft  []*Plot // plots using the left Y axis
+	GroupName  string  // for internal caching in Canvas
+}
+
+func NewGraph(kind Kind, name string) *Graph {
+	return &Graph{
+		Kind:   kind,
+		Name:   name,
+		Title:  name,
+		XLabel: "Value",
+	}
+}
+
+func (g *Graph) SetTitle(t string) *Graph {
+	g.Title = t
+	return g
+}
+
+func (g *Graph) SetXLabel(l string) *Graph {
+	g.XLabel = l
+	return g
+}
+
+func (g *Graph) SetYLogScale(b bool) *Graph {
+	g.YLogScale = b
+	return g
+}
+
+// AddPlotRight adds a plot to be displayed using the right Y axis. It's an
+// error if the plot and the Graph have different Kinds.
+func (g *Graph) AddPlotRight(p *Plot) error {
+	if p.Kind != g.Kind {
+		return errors.Reason("plot's kind [%s] != graph's kind [%s]",
+			p.Kind, g.Kind)
+	}
+	g.PlotsRight = append(g.PlotsRight, p)
+	return nil
+}
+
+// AddPlotLeft adds a plot to be displayed using the left Y axis. It's an error
+// if the plot and the Graph have different Kinds.
+func (g *Graph) AddPlotLeft(p *Plot) error {
+	if p.Kind != g.Kind {
+		return errors.Reason("plot's kind [%s] != graph's kind [%s]",
+			p.Kind, g.Kind)
+	}
+	g.PlotsLeft = append(g.PlotsLeft, p)
+	return nil
+}
+
+// Group of Graphs sharing the same X axis. Visually, all graphs should be
+// displayed one after another in the given order, having the same width and
+// aligned vertically, to match their X axis positions.
+type Group struct {
+	Kind      Kind              // must match Graph's Kind
+	Name      string            // internal unique name of the group
+	XLogScale bool              // whether to use log-scale for X axis
+	Graphs    []*Graph          // to preserve the order
+	graphMap  map[string]*Graph // for quick access
+}
+
+func NewGroup(kind Kind, name string) *Group {
+	return &Group{
+		Kind:     kind,
+		Name:     name,
+		graphMap: make(map[string]*Graph),
+	}
+}
+
+func (g *Group) SetXLogScale(b bool) *Group {
+	g.XLogScale = b
+	return g
+}
+
+// addGraph to both the slice and the map, and update graph's GroupName, to keep
+// all of them in sync.
+func (g *Group) addGraph(graph *Graph) {
+	g.Graphs = append(g.Graphs, graph)
+	g.graphMap[graph.Name] = graph
+	graph.GroupName = g.Name
+}
+
+// AddGraph to the Group. It's an error if the Graph Kind doesn't match the
+// Group Kind.
+func (g *Group) AddGraph(graph *Graph) error {
+	if g.Kind != graph.Kind {
+		return errors.Reason("group's Kind [%s] != graph's Kind [%s]",
+			g.Kind, graph.Kind)
+	}
+	if _, ok := g.graphMap[graph.Name]; ok {
+		return errors.Reason("graph %s already exists in group %s",
+			graph.Name, g.Name)
+	}
+	g.addGraph(graph)
+	return nil
+}
+
+// Canvas is the master collection for all the plot groups
+type Canvas struct {
+	Groups   []*Group          // to preserve the order
+	groupMap map[string]*Group // for quick reference by name
+	graphMap map[string]*Graph // direct Graph reference within Groups
+}
+
+func NewCanvas() *Canvas {
+	return &Canvas{
+		groupMap: make(map[string]*Group),
+		graphMap: make(map[string]*Graph),
+	}
+}
+
+// addGroup to both the slice and the map, to keep them in sync.
+func (c *Canvas) addGroup(group *Group) {
+	c.Groups = append(c.Groups, group)
+	c.groupMap[group.Name] = group
+}
+
+func (c *Canvas) AddGroup(group *Group) error {
+	if _, ok := c.groupMap[group.Name]; ok {
+		return errors.Reason("group %s already exists in Canvas", group.Name)
+	}
+	// First, just check for graph duplicates, not to modify Canvas in case of an
+	// error. We assume that graphs in the group all have different names.
+	for name := range group.graphMap {
+		if _, ok := c.graphMap[name]; ok {
+			return errors.Reason("graph %s in group %s already exists in Canvas",
+				name, group.Name)
+		}
+	}
+	c.addGroup(group)
+	for name, graph := range group.graphMap {
+		c.graphMap[name] = graph
+	}
+	return nil
+}
+
+// GetGroup by name, if it exists, otherwise nil.
+func (c *Canvas) GetGroup(name string) *Group {
+	g, ok := c.groupMap[name]
+	if !ok {
+		return nil
+	}
+	return g
+}
+
+// AddGraph to the group by name. If the group doesn't exist, it is created with
+// the same Kind as the Graph.
+func (c *Canvas) AddGraph(graph *Graph, groupName string) error {
+	if _, ok := c.graphMap[graph.Name]; ok {
+		return errors.Reason("graph %s already exists in Canvas", graph.Name)
+	}
+	group, ok := c.groupMap[groupName]
+	if !ok {
+		group = NewGroup(graph.Kind, groupName)
+		c.addGroup(group)
+	}
+	if err := group.AddGraph(graph); err != nil {
+		return errors.Annotate(err, "failed to add graph %s to group %s",
+			graph.Name, groupName)
+	}
+	c.graphMap[graph.Name] = graph
+	return nil
+}
+
+// GetGraph by name, if it exists, otherwise nil.
+func (c *Canvas) GetGraph(name string) *Graph {
+	g, ok := c.graphMap[name]
+	if !ok {
+		return nil
+	}
+	return g
+}
+
+// EnsureGraph creates the requested Graph and/or Group as necessary, and makes
+// sure that the existing graph is indeed in the requested group. If the graph
+// exists but in the wrong group, it's an error. Returns the graph which can be
+// used for further configuration.
+func (c *Canvas) EnsureGraph(kind Kind, graphName, groupName string) (*Graph, error) {
+	if graph, ok := c.graphMap[graphName]; ok {
+		if graph.Kind != kind {
+			return nil, errors.Reason("graph %s has kind %s != required kind %s",
+				graphName, graph.Kind, kind)
+		}
+		if graph.GroupName != groupName {
+			return nil, errors.Reason(
+				"cannot ensure graph %s in group %s: it already exists in group %s",
+				graphName, groupName, graph.GroupName)
+		}
+		return graph, nil
+	}
+	graph := NewGraph(kind, graphName)
+	if err := c.AddGraph(graph, groupName); err != nil {
+		return nil, errors.Annotate(err, "cannot ensure graph %s in group %s",
+			graphName, groupName)
+	}
+	return graph, nil
+}
+
+// AddPlotRight to the graph by name to be displayed using the right Y axis. The
+// graph must exist in Canvas.
+func (c *Canvas) AddPlotRight(p *Plot, graphName string) error {
+	graph, ok := c.graphMap[graphName]
+	if !ok {
+		return errors.Reason("no such graph in Canvas: %s", graphName)
+	}
+	if err := graph.AddPlotRight(p); err != nil {
+		return errors.Annotate(err, "failed to add plot %s to Canvas", p.Legend)
+	}
+	return nil
+}
+
+// AddPlotLeft to the graph by name to be displayed using the left Y axis. The
+// graph must exist in Canvas.
+func (c *Canvas) AddPlotLeft(p *Plot, graphName string) error {
+	graph, ok := c.graphMap[graphName]
+	if !ok {
+		return errors.Reason("no such graph in Canvas: %s", graphName)
+	}
+	if err := graph.AddPlotLeft(p); err != nil {
+		return errors.Annotate(err, "failed to add plot %s to Canvas", p.Legend)
+	}
+	return nil
+}
---
+++ plot/plot_test.go
@@ -0,0 +1,156 @@
+// Copyright 2022 Stock Parfait
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plot
+
+import (
+	"testing"
+
+	"github.com/stockparfait/stockparfait/db"
+	"github.com/stockparfait/stockparfait/stats"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPlot(t *testing.T) {
+	t.Parallel()
+
+	Convey("Plot API works", t, func() {
+		c := NewCanvas()
+		xyGroup := NewGroup(XYKind, "xy").SetXLogScale(true)
+		seriesGroup := NewGroup(SeriesKind, "series")
+
+		So(xyGroup.XLogScale, ShouldBeTrue)
+		So(c.AddGroup(xyGroup), ShouldBeNil)
+		So(c.AddGroup(seriesGroup), ShouldBeNil)
+		So(c.AddGroup(NewGroup(XYKind, "xy")), ShouldNotBeNil)
+		So(c.Groups, ShouldResemble, []*Group{xyGroup, seriesGroup})
+
+		Convey("Adding graphs", func() {
+			Convey("to existing groups", func() {
+				pdfGraph, err := c.EnsureGraph(XYKind, "p.d.f.", "xy")
+				So(err, ShouldBeNil)
+				pdfGraph.SetTitle("Distributions").SetXLabel("price").SetYLogScale(true)
+
+				timeGraph, err := c.EnsureGraph(SeriesKind, "time", "series")
+				So(err, ShouldBeNil)
+
+				So(pdfGraph.Title, ShouldEqual, "Distributions")
+				So(pdfGraph.XLabel, ShouldEqual, "price")
+				So(pdfGraph.YLogScale, ShouldBeTrue)
+				So(pdfGraph.GroupName, ShouldEqual, "xy")
+
+				// Correct graph exists.
+				g, err := c.EnsureGraph(XYKind, "p.d.f.", "xy")
+				So(err, ShouldBeNil)
+				So(g, ShouldEqual, pdfGraph)
+
+				_, err = c.EnsureGraph(SeriesKind, "wrong", "xy")
+				So(err, ShouldNotBeNil)
+				// A graph with the wrong kind already exists.
+				_, err = c.EnsureGraph(XYKind, "time", "xy")
+				So(err, ShouldNotBeNil)
+
+				// Duplicate graph name in another group.
+				_, err = c.EnsureGraph(SeriesKind, "time", "xy")
+				So(err, ShouldNotBeNil)
+
+				So(len(c.graphMap), ShouldEqual, 2)
+				So(len(c.Groups), ShouldEqual, 2)
+				So(c.Groups[0].Graphs[0], ShouldEqual, pdfGraph)
+				So(c.Groups[1].Graphs[0], ShouldEqual, timeGraph)
+			})
+
+			Convey("to a new group", func() {
+				g, err := c.EnsureGraph(XYKind, "scatter", "dots")
+				So(err, ShouldBeNil)
+				So(len(c.Groups), ShouldEqual, 3)
+				So(c.Groups[:2], ShouldResemble, []*Group{xyGroup, seriesGroup})
+				So(c.Groups[2].Graphs[0], ShouldEqual, g)
+			})
+
+			Convey("from a group pre-populated with graphs", func() {
+				gr := NewGroup(XYKind, "prep")
+				g1 := NewGraph(XYKind, "one")
+				g2 := NewGraph(XYKind, "two")
+
+				So(gr.AddGraph(g1), ShouldBeNil)
+				So(gr.AddGraph(g2), ShouldBeNil)
+				So(gr.AddGraph(NewGraph(XYKind, "one")), ShouldNotBeNil)
+
+				So(c.AddGroup(gr), ShouldBeNil)
+				So(c.Groups, ShouldResemble, []*Group{xyGroup, seriesGroup, gr})
+				So(len(c.graphMap), ShouldEqual, 2)
+
+				So(c.GetGroup("prep"), ShouldEqual, gr)
+				So(c.GetGraph("one"), ShouldEqual, g1)
+			})
+		})
+
+		Convey("Adding plots", func() {
+			g1, err := c.EnsureGraph(XYKind, "lines", "xy")
+			So(err, ShouldBeNil)
+			So(g1.Name, ShouldEqual, "lines")
+
+			_, err = c.EnsureGraph(SeriesKind, "prices", "time")
+			So(err, ShouldBeNil)
+
+			x := []float64{1.0, 2.0, 3.0}
+			y := []float64{10.0, 20.0, 30.0}
+			dates := []db.Date{
+				db.NewDate(2020, 1, 1),
+				db.NewDate(2020, 1, 2),
+				db.NewDate(2020, 1, 3),
+			}
+			ts := stats.NewTimeseries().Init(dates, y)
+
+			xyPlot := NewXYPlot(x, y).
+				SetYLabel("p").
+				SetLegend("PDF").
+				SetChartType(ChartDashed)
+
+			timePlot := NewSeriesPlot(ts)
+
+			Convey("access methods work", func() {
+				x2, y2 := xyPlot.GetXY()
+				So(x2, ShouldResemble, x)
+				So(y2, ShouldResemble, y)
+				So(func() { xyPlot.GetTimeseries() }, ShouldPanic)
+
+				So(func() { timePlot.GetXY() }, ShouldPanic)
+				So(timePlot.GetTimeseries(), ShouldResemble, ts)
+			})
+
+			Convey("add to the right Y axis", func() {
+				So(c.AddPlotRight(xyPlot, "lines"), ShouldBeNil)
+				So(c.AddPlotRight(timePlot, "prices"), ShouldBeNil)
+				So(c.AddPlotRight(timePlot, "lines"), ShouldNotBeNil)
+				So(c.AddPlotRight(xyPlot, "nonexistent"), ShouldNotBeNil)
+
+				So(c.graphMap["lines"].PlotsRight, ShouldResemble, []*Plot{xyPlot})
+				So(c.graphMap["prices"].PlotsRight, ShouldResemble, []*Plot{timePlot})
+			})
+
+			Convey("add to the left Y axis", func() {
+				So(c.AddPlotLeft(xyPlot, "lines"), ShouldBeNil)
+				So(c.AddPlotLeft(timePlot, "prices"), ShouldBeNil)
+				So(c.AddPlotLeft(timePlot, "lines"), ShouldNotBeNil)
+				So(c.AddPlotLeft(xyPlot, "nonexistent"), ShouldNotBeNil)
+
+				So(c.graphMap["lines"].PlotsLeft, ShouldResemble, []*Plot{xyPlot})
+				So(c.graphMap["prices"].PlotsLeft, ShouldResemble, []*Plot{timePlot})
+			})
+		})
+	})
+}
